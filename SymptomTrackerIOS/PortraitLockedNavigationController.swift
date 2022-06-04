5f713fbb8b0307eeb7a5728681752b81ad471435
Kira Nim <kiras.nim@gmail.com>
2022-06-04 02:27:38 +0200

    Update comments before sleeeeeep :D

--- SymptomTrackerIOS/PortraitLockedNavigationController.swift
+++ SymptomTrackerIOS/PortraitLockedNavigationController.swift
@@ -8,17 +8,24 @@
 import Foundation
 import UIKit
 
+// Because we want to be able to lock the device to only support portrait orientation when using the app
+// apart from one page (Insight). We need to override som of the functionalyty available in UINavigationController.
 class PortraintLockedNavigationController: UINavigationController {
     
-    
+    // Is called by OS when the phone has changed orientation.
+    // Responsible for telling OS whether the screen should be turned if the phone is turned.
+    // Default will return true (in the supers implementation)
+    // "UIDevice.current.orientation" Is the position the phone has changed to, when this method is called by OS.
     override var shouldAutorotate: Bool {
         return UIDevice.current.orientation == .portrait
     }
 
+    // This function is called IF the above function is called. This is a bit wierd but it works
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.portrait
     }
 
+    // Same as above but when presenting a controller modaly (pop up from below)
     override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
         return UIInterfaceOrientation.portrait
     }
--- SymptomTrackerIOS/UITabbarController+OrientationHandling.swift
+++ SymptomTrackerIOS/UITabbarController+OrientationHandling.swift
@@ -8,8 +8,10 @@
 import Foundation
 import UIKit
 
-//
-
+// This is an extension on our tabbar controller (UITabBarController).
+// This functionality makes sure that when OS acceses the below propertien on a (our one) tabbarController
+// Then the child controller of that tabbar wil be contacted instead
+// SelectedViewController is the child of the tabbar that is responsible for the view at a given time (selected tabbar navigationController so to speak)
 extension UITabBarController {
     public override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if let selected = selectedViewController {
